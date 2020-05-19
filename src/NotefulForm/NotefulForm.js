c9e9906eeefbc6697e65562920c5e673dc70e596
Chayce Knaub <chknaub@yahoo.com>
2020-05-18 20:50:16 -0400

    comment added

--- src/NotefulForm/NotefulForm.js
+++ src/NotefulForm/NotefulForm.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import './NotefulForm.css'
-
+import React from "react";
+import "./NotefulForm.css";
+// check git
 export default function NotefulForm(props) {
-  const { className, ...otherProps } = props
+  const { className, ...otherProps } = props;
   return (
     <form
-      className={['Noteful-form', className].join(' ')}
-      action='#'
+      className={["Noteful-form", className].join(" ")}
+      action="#"
       {...otherProps}
     />
-  )
+  );
 }
